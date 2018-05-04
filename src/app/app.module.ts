40fb0a9cb76554e67f4d41e793ccc574aba5d55a
angemarie <ange-m.tellier@laposte.net>
2018-05-04 13:34:57 +0200

    Revert project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { LoginPage } from '../pages/login/login';
+import { DevisListPage } from "../pages/devis-list/devis-list";
+import { DevisFormPage } from "../pages/devis-form/devis-form";
+
+import { UserProvider } from '../providers/user/user';
+import { DevisProvider } from '../providers/devis/devis';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LoginPage,
+    DevisListPage,
+    DevisFormPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +27,16 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage,
+    DevisListPage,
+    DevisFormPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserProvider,
+    DevisProvider
   ]
 })
 export class AppModule {}
